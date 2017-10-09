e0ca4f3215ab5cd442d1d4dd568f9d6e2364d8f2
Simon Struk <simon.struk@gmail.com>
2017-10-09 02:03:13 +0200

    Add 2 basic RLP tests
    - a skeleton of class RLP
    - a test for initialization from insufficient data
    - a test for parsing the first level of RLP tree
    
    Signed-off-by: Simon Struk <simon.struk@gmail.com>

--- src/rlp.h
+++ src/rlp.h
@@ -1,14 +1,39 @@
 #ifndef RLP_H
 #define RLP_H
 
+#include <exception>
+#include <vector>
+
+class BadFormat : public std::exception
+{
+public:
+	virtual const char* what() const throw()
+	{
+		return "Bad RLP format";
+	}
+};
+
 class RLP
 {
-	int bar;
+	unsigned char* _data;
+	size_t _dataLength;
+	std::vector<RLP> _items;
 
 public:
-	RLP(int x) { bar = x; }
 
-	int foo() { return bar; }
+	RLP(unsigned char* data, size_t maxLength)
+		: _data{data}, _dataLength{maxLength}
+	{
+		throw BadFormat();
+	}
+
+	size_t length() const { return 0; }
+
+	const RLP & operator[](unsigned int index) const;
+
+	unsigned int numItems() const { return 0; }
+
+	const unsigned char* data() const { return _data; }
 };
 
 #endif
--- test/main.cpp
+++ test/main.cpp
@@ -1,20 +1,55 @@
 #include "gtest/gtest.h"
 #include "rlp.h"
 
-TEST(FooTest, ConstructorTest)
+#include <boost/interprocess/file_mapping.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+
+using namespace std;
+using namespace boost::interprocess;
+
+class RLPTest : public ::testing::Test
 {
-	RLP rlp(4);
-	EXPECT_EQ(4, rlp.foo());
+protected:
+	RLPTest()
+	{
+		const char* filename = "../test/samples/1000000";
+		file_mapping m_file(filename, read_only);
+		mapped_region f(m_file,read_only);
+
+		data_ = (unsigned char *) f.get_address();
+		maxLength_ = f.get_size();
+	}
+	
+	unsigned char* data_;
+	size_t maxLength_;
+};
+
+
+TEST_F(RLPTest, ConstructorNotEnoughData)
+{
+	try {
+		RLP(data_, maxLength_ - 1);
+		FAIL() << "Expected BadFormat exception";
+
+	} catch(BadFormat const & err) {
+		EXPECT_STREQ(err.what(), "Bad RLP format");
+
+	} catch(...) {
+		FAIL() << "Expected BadFormat exception";
+	}
 }
 
-TEST(FooTest, ConstructorFailTest)
+TEST_F(RLPTest, FirstLevelParseTest)
 {
-	RLP rlp(4);
-	EXPECT_EQ(5, rlp.foo());
+	RLP block(data_, maxLength_);
+	EXPECT_EQ(0xF9, block.data()[0]);
+	EXPECT_EQ(768, block.length());
+	EXPECT_EQ(3, block.numItems());
 }
+
 
 int main(int argc, char* argv[])
 {
-	testing::InitGoogleTest(&argc, argv);
+	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
