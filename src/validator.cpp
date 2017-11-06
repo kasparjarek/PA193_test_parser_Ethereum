82eb89c00637566baa360a9cc1c07be987a72ca2
Vojta Polasek <410266@mail.muni.cz>
2017-11-06 12:03:44 +0100

    rewrote validating function to take parameters by reference fixed difficulty validation added gaslimit validation

--- src/validator.cpp
+++ src/validator.cpp
@@ -1,5 +1,6 @@
 #include"block.h"
 #include<tgmath.h>
+//#include<gcrypt.h>
 
 using namespace std;
 
@@ -10,9 +11,9 @@ using namespace std;
  * @ param Block
  * @return int
 */
-int validateParentHash(const Block b1, const Block b2) {
+int validateParentHash(const Block& parent, const Block& child) {
     vector<uint8_t> hash1; // place holder, will be replaced by hash of root node of previous node
-    vector<uint8_t> hash2 = b2.header().parentHash();
+    vector<uint8_t> hash2 = child.header().parentHash();
     if (hash1 == hash2) {
             return 0;
     }
@@ -21,8 +22,8 @@ int validateParentHash(const Block b1, const Block b2) {
     }
 }
 
-int validateBlockNumber(const Block b1, const Block b2) {
-    if (b1.header().number() == (b2.header().number() - 1)) {
+int validateBlockNumber(const Block& parent, const Block& child) {
+    if (parent.header().number() == (child.header().number() - 1)) {
             return 0;
     }
     else {
@@ -31,9 +32,10 @@ int validateBlockNumber(const Block b1, const Block b2) {
 }
 
 //difficulty formula taken from https://ethereum.stackexchange.com/questions/1880/how-is-the-mining-difficulty-calculated-on-ethereum
-int validateDifficulty(const Block b1, const Block b2) {
-    size_t block_diff = b1.header().difficulty() + b1.header().difficulty() / 2048 * fmax(1 - (b2.header().timestamp() - b1.header().timestamp()) / 10, -99) + pow(2, ((b2.header().number() / 100000) - 2));
-    if (block_diff == b2.header().difficulty()) {
+//doplnit že se bere maximum ze vzorce a 131072
+int validateDifficulty(const Block& parent, const Block& child) {
+    size_t block_diff = fmax(parent.header().difficulty() + parent.header().difficulty() / 2048 * fmax(1 - (child.header().timestamp() - parent.header().timestamp()) / 10, -99) + pow(2, ((child.header().number() / 100000) - 2)), 131072);
+    if (block_diff == child.header().difficulty()) {
             return 0;
     }
     else {
@@ -41,35 +43,40 @@ int validateDifficulty(const Block b1, const Block b2) {
     }
 }
 
-int validateGaslimit(const Block b1, const Block b2) {
+int validateGaslimit(const Block& parent, const Block& child) {
+    if ((child.header().gasLimit() < parent.header().gasLimit() + (parent.header().gasLimit() / 1024)) && (child.header().gasLimit() > parent.header().gasLimit() - (parent.header().gasLimit() / 1024))) {
+            return 0;
+    }
+    else {
+            return 1;
+    }
+}
+
+int validateTimestamp(const Block& parent, const Block& child) {
+    if (child.header().timestamp() > parent.header().timestamp()) {
+            return 0;
+    }
+    else {
+            return 1;
+    }
+}
+
+int validateNonce(const Block& parent, const Block& child) {
+    /*This method is not implemented because proper verification of nonce requires download whole state database. We think that downloading of whole state database and parsing it is a bit over the main goal of this project.*/
     return 0;
 }
 
-int validateTimestamp(const Block b1, const Block b2) {
-    if (b2.header().timestamp() > b1.header().timestamp()) {
-            return 0;
-    }
-    else {
-            return 1;
-    }
-}
-
-int validateNonce(const Block b1, const Block b2) {
-    /*This method is not implemented because proper verification of nonce requires download whole state database. We think that downloading of whole state database and parsing it is a bit over the main goal of this project.*/
-    return 1;
-}
-
-int validateMixHash(const Block b1, const Block b2); {
+int validateMixHash(const Block& parent, const Block& child); {
     /*This method is not implemented because proper verification of mixHash requires download whole state database. We think that downloading of whole state database and parsing it is a bit over the main goal of this project.*/
-    return 1;
+    return 0;
 }
 
-int validateReceipts(Const Block b1, const Block b2) {
+int validateReceipts(Const Block parent, const Block child) {
     /*This method is not implemented because proper verification of receipts trie requires download whole state database. We think that downloading of whole state database and parsing it is a bit over the main goal of this project.*/
-    return 1;
+    return 0;
 }
 
-int validateLogsBloom(const Block b1, const Block b2) {
+int validateLogsBloom(const Block& parent, const Block& child) {
     /*This method is not implemented because we would need to implement or use EVM (Ethereum virtual machine). We think that this is strongly out of scope of this project. */
-    return 1;
+    return 0;
 }
