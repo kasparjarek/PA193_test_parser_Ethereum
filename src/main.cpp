1246d69163d9f681356055e303797f444fe08f4f
Vojta Polasek <410266@mail.muni.cz>
2017-11-08 18:52:21 +0100

    cleaned up main

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,7 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	/*vector<uint8_t> d1{4,5,6,23,56,4,5,4,3,6,7,23,6,9};
-	vector<uint8_t> d2{4,5,23,6,9};
-	vector<uint8_t> d3{};
-	vector<uint8_t> d4{4,5,6,23,56,4,5,4,3,6,7,23,6,9,5,6,23,56,4,5,4,3,6,7,23,6,9,5,6,23,56,4,5,4,3,6,7,23,6,9,5,6,23,56,4,5,4,3,6,7,23,6,9,5,6,23,56,4,5,4,3,6,7,23,6,9,5,6,23,56,4,5,4,3,6,7,23,6,9,5,6,23,56,4,5,4,3,6,7,23,6,9,5,6,23,56,4,5,4,3,6,7,23,6,9,5,6,23,56,4,5,4,3,6,7,23,6,9,5,6,23,56,4,5,4,3,6,7,23,6,9};
-	vector<uint8_t> d5{128};
-	vector<uint8_t> d6{127};
-
-	cout << "a" << endl;
-	//vector<uint8_t> serialized = RLP::serialize(vector<vector<uint8_t> > {d1, d2, d3, d4, d5, d6});
-	cout << "b" << endl;
-	//for (auto x : serialized) {
-	//	printf("%d ", x);
-	//}
-	printf("\n");
-
-	//RLP r{serialized};
-	//printf("num_items: %d\n", r.numItems());
-	//for (unsigned i = 0; i < r.numItems(); ++i) {
-		//for (unsigned j = 0; j < r[i].dataLength(); ++j) {
-			//printf("%d ", serialized[r[i].dataOffset() + j]);
-		//}
-		//printf("\n");
-	//}
-	 * */
-    if (argc != 3) {
+	    if (argc != 3) {
             cout << "The program expects exactly 2 arguments." << endl << "Both arguments have to be valid file paths to Ethereum binary blocks." << endl;
             return 1;
     }
@@ -55,12 +31,6 @@ int main(int argc, char **argv)
     }
     Block childblock = parser.blocks()[0];
     validateAll(parentblock, childblock);
-
-    vector<uint8_t> b = parentblock.toRLP();
-    for (auto x : b)
-	    cout << hex << int(x) << " ";
-    cout <<endl;
-
     return 0;
 }
 
